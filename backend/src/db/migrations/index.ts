98bf0df8609574aa207a12f4b79bff852fecd4de
Albert Asaftei <albert.asaftei00@gmail.com>
2026-01-31 22:11:02 +0100

    fix: :green_heart: missing extension for tsc build

--- backend/src/db/migrations/index.ts
+++ backend/src/db/migrations/index.ts
@@ -1,5 +1,5 @@
 import type { Migration } from "../migrations";
-import { addDeletedDocuments } from "./001-add-deleted-documents";
+import { addDeletedDocuments } from "./001-add-deleted-documents.js";
 
 // All migrations in order - put them inside this folder and add them here
 export const allMigrations: Migration[] = [addDeletedDocuments];
